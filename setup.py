3fd4969aa40335c3f0f0fac951cf486a84f62e64
jagrutishinde <shindejagruti@gmail.com>
2023-12-20 15:44:21 +0530

    setup files updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from setuptools import find_packages,setup
+
+setup(
+name = 'Medical Chatbot',
+version= "0.0.0",
+author = "JagrutiShinde",
+author_email= "shindejagruti@gmail.com",
+packages=find_packages(),
+install_requires = []
+
+)
+
--- template.py
+++ template.py
@@ -11,7 +11,7 @@ list_of_files = [
     "src/helper.py",
     "src/prompt.py",
     ".env",
-    "setuo.py",
+    "setup.py",
     "research/trials.ipynb",
     "app.py",
     "store_index.py",
